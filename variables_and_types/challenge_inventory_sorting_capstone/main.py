3fb166bd71182526b8ce48f4564c0f87a233b8f9
tapimucheri-rgb <>
2026-03-27 12:26:14 +0000

    Challenge: Inventory Sorting Capstone

--- variables_and_types/challenge_inventory_sorting_capstone/main.py
+++ variables_and_types/challenge_inventory_sorting_capstone/main.py
@@ -9,3 +9,6 @@ category2 = categories[13:25]
 bubblegum_price = "$1.50"
 chocolate_price = "$2.00"
 pasta_price ="$5.40"
+print("we have", candy1, "for", bubblegum_price, "in the", category1)
+print("we have", candy2, "for", chocolate_price, "in the", category2)
+print("we have", dry_goods, "for", )
